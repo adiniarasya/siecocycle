9740d44a3ad15f0075a214f9fc09a4173f81013c
Aqilla <aqilla.oktaviani214@smk.belajar.id>
2026-04-30 13:41:22 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,18 +32,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
-    <div id="app">
-        <section class="section">
-            <div class="container mt-5">
-                <div class="row">
-                    <div
-                        class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                        <div class="login-brand">
-                            <img src="assets/img/stisla-fill.svg" alt="logo" width="100"
-                                class="shadow-light rounded-circle">
-                        </div>
-=======
   <div id="app">
     <section class="section">
       <div class="container mt-5">
@@ -52,7 +40,6 @@
             <div class="login-brand">
               <img src="{{ asset('Stisla/dist/assets/img/logoeco.jpeg')}}" alt="logo" width="100" class="shadow-light rounded-circle">
             </div>
->>>>>>> c79e4c4eb5c7cb3c65d918600094b2376175199c
 
                         <div class="card card-primary">
                             <div class="card-header">
